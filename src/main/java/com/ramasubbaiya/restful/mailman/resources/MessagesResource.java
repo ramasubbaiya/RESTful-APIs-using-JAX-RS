94b0afc920b5fc89f96667543806f752e1a71725
Ramasubbaiya Adaikkalam <ramasubbaiya@gmail.com>
2016-05-30 00:46:15 -0400

    Implementing POST PUT DELETE

--- src/main/java/com/ramasubbaiya/restful/mailman/resources/MessagesResource.java
+++ src/main/java/com/ramasubbaiya/restful/mailman/resources/MessagesResource.java
@@ -2,7 +2,11 @@ package com.ramasubbaiya.restful.mailman.resources;
 
 import java.util.List;
 
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -21,6 +25,30 @@ public class MessagesResource {
 	public List<Message> getMessages() {
 		return messageService.getAllMessages();
 	}
+	
+	@POST
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	public Message addMessage(Message message) {
+		return messageService.addMessage(message);
+	}
+	
+	@PUT
+	@Path("/{messageId}")
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	public Message updateMessage(@PathParam("messageId") long id, Message message) {
+		message.setId(id);
+		return messageService.updateMessage(message);
+	}
+	
+	@DELETE
+	@Path("/{messageId}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Message removeMessage(@PathParam("messageId") long messageId) {
+		return messageService.removeMessage(messageId);
+	}
+	
 	
 	@GET
 	@Path("/{messageId}")
