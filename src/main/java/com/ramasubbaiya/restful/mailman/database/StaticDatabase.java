8b4bb811380d2d7c88d7526f057999b612ab0419
Ramasubbaiya Adaikkalam <ramasubbaiya@gmail.com>
2016-05-30 13:32:20 -0400

    creating instances of messages in StaticDatabase

--- src/main/java/com/ramasubbaiya/restful/mailman/database/StaticDatabase.java
+++ src/main/java/com/ramasubbaiya/restful/mailman/database/StaticDatabase.java
@@ -12,6 +12,9 @@ public class StaticDatabase {
 	private static Map<Long, Message> profiles = new HashMap<>();
 	
 	public static Map<Long, Message> getMessages() {
+		messages.put(1L, new Message(1, "Message 1 ", "Rama"));
+		messages.put(2L, new Message(2, "Message 2 ", "Rama"));
+		messages.put(3L, new Message(3, "Message 3 ", "Rama"));
 		return messages;
 	}
 	
